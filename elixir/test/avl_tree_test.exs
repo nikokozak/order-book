0d6fa846785789661c5ce13cadd61ed889582ff3
Nikolai Kozak <nikokozak@gmail.com>
2022-02-09 19:02:22 -0300

    add orderqueue, pricetree structs

--- elixir/test/avl_tree_test.exs
+++ elixir/test/avl_tree_test.exs
@@ -1,16 +1,17 @@
-defmodule AVLTreeTest do
+defmodule PriceTreeTest do
   use ExUnit.Case
+  alias OrderBook.PriceTree
 
-  doctest AVLTree, except: [view: 1]
+  doctest PriceTree, except: [view: 1]
 
   use ExUnit.Case
 
   test "protocols" do
-    tree1 = Enum.into([5, 9, 3, 8, 1, 6, 7], AVLTree.new())
+    tree1 = Enum.into([5, 9, 3, 8, 1, 6, 7], PriceTree.new())
     assert Enum.to_list(tree1) == [1, 3, 5, 6, 7, 8, 9]
     assert Enum.member?(tree1, 6)
     assert !Enum.member?(tree1, 10)
-    tree2 = Enum.into([8, 4, 2, 5, 7, 2, 1, 3], AVLTree.new())
+    tree2 = Enum.into([8, 4, 2, 5, 7, 2, 1, 3], PriceTree.new())
     assert Enum.to_list(tree2) == [1, 2, 2, 3, 4, 5, 7, 8]
     assert Enum.zip(tree1, tree2) == [{1, 1}, {3, 2}, {5, 2}, {6, 3}, {7, 4}, {8, 5}, {9, 7}]
   end
@@ -19,21 +20,21 @@ defmodule AVLTreeTest do
     asc = &Kernel.</2
     desc = &Kernel.>/2
 
-    %AVLTree{root: nil, size: 0, less: ^asc} = AVLTree.new()
-    %AVLTree{root: nil, size: 0, less: ^asc} = AVLTree.new(:asc)
-    %AVLTree{root: nil, size: 0, less: ^desc} = AVLTree.new(:desc)
+    %PriceTree{root: nil, size: 0, less: ^asc} = PriceTree.new()
+    %PriceTree{root: nil, size: 0, less: ^asc} = PriceTree.new(:asc)
+    %PriceTree{root: nil, size: 0, less: ^desc} = PriceTree.new(:desc)
   end
 
   test "put" do
-    tree = Enum.into([5, 9, 3, 8, 1, 6, 7], AVLTree.new())
+    tree = Enum.into([5, 9, 3, 8, 1, 6, 7], PriceTree.new())
     assert Enum.to_list(tree) == [1, 3, 5, 6, 7, 8, 9]
-    assert AVLTree.member?(tree, 3)
-    assert !AVLTree.member?(tree, 2)
+    assert PriceTree.member?(tree, 3)
+    assert !PriceTree.member?(tree, 2)
 
-    assert AVLTree.size(tree) == 7
-    assert AVLTree.height(tree) == 4
+    assert PriceTree.size(tree) == 7
+    assert PriceTree.height(tree) == 4
 
-    assert inspect(tree) == "#AVLTree<[1, 3, 5, 6, 7, 8, 9]>"
+    assert inspect(tree) == "#PriceTree<[1, 3, 5, 6, 7, 8, 9]>"
 
     #    5
     #  ┌─┴───┐
@@ -48,8 +49,8 @@ defmodule AVLTreeTest do
 
     tree = Enum.into([11, 10, 12], tree)
     assert Enum.to_list(tree) == [1, 3, 5, 6, 7, 8, 9, 10, 11, 12]
-    assert AVLTree.size(tree) == 10
-    assert AVLTree.height(tree) == 4
+    assert PriceTree.size(tree) == 10
+    assert PriceTree.height(tree) == 4
 
     #        8
     #    ┌───┴─┐
@@ -62,10 +63,10 @@ defmodule AVLTreeTest do
              {8, 4, {5, 3, {3, 2, {1, 1, nil, nil}, nil}, {6, 2, nil, {7, 1, nil, nil}}},
               {10, 3, {9, 1, nil, nil}, {11, 2, nil, {12, 1, nil, nil}}}}
 
-    tree = AVLTree.put(tree, 4)
+    tree = PriceTree.put(tree, 4)
     assert Enum.to_list(tree) == [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    assert AVLTree.size(tree) == 11
-    assert AVLTree.height(tree) == 4
+    assert PriceTree.size(tree) == 11
+    assert PriceTree.height(tree) == 4
 
     #        8
     #    ┌───┴─┐
@@ -79,64 +80,64 @@ defmodule AVLTreeTest do
               {5, 3, {3, 2, {1, 1, nil, nil}, {4, 1, nil, nil}}, {6, 2, nil, {7, 1, nil, nil}}},
               {10, 3, {9, 1, nil, nil}, {11, 2, nil, {12, 1, nil, nil}}}}
 
-    assert tree == AVLTree.put(tree, 7)
+    assert tree == PriceTree.put(tree, 7)
   end
 
   test "get" do
     tree =
       [{5, 50}, {1, 10}, {9, 90}, {1, 20}, {5, 40}, {7, 70}, {2, 20}]
-      |> Enum.into(AVLTree.new(fn {k1, _}, {k2, _} -> k1 < k2 end))
+      |> Enum.into(PriceTree.new(fn {k1, _}, {k2, _} -> k1 < k2 end))
 
-    assert AVLTree.get(tree, {7, nil}) == {7, 70}
-    assert AVLTree.get(tree, {9, nil}) == {9, 90}
-    assert AVLTree.get(tree, {5, nil}) == {5, 50}
-    assert AVLTree.get(tree, {1, nil}) == {1, 20}
-    assert AVLTree.get(tree, {6, nil}) == nil
-    assert AVLTree.get(tree, {6, nil}, :error) == :error
+    assert PriceTree.get(tree, {7, nil}) == {7, 70}
+    assert PriceTree.get(tree, {9, nil}) == {9, 90}
+    assert PriceTree.get(tree, {5, nil}) == {5, 50}
+    assert PriceTree.get(tree, {1, nil}) == {1, 20}
+    assert PriceTree.get(tree, {6, nil}) == nil
+    assert PriceTree.get(tree, {6, nil}, :error) == :error
   end
 
   test "get first/last" do
     tree =
       [{5, 50}, {1, 10}, {9, 90}, {1, 20}, {5, 40}, {7, 70}, {2, 20}]
-      |> Enum.into(AVLTree.new(fn {k1, _}, {k2, _} -> k1 < k2 end))
+      |> Enum.into(PriceTree.new(fn {k1, _}, {k2, _} -> k1 < k2 end))
 
-    assert AVLTree.get_first(tree) == {1, 10}
-    assert AVLTree.get_last(tree) == {9, 90}
+    assert PriceTree.get_first(tree) == {1, 10}
+    assert PriceTree.get_last(tree) == {9, 90}
 
-    tree = AVLTree.new()
-    assert AVLTree.get_first(tree) == nil
-    assert AVLTree.get_first(tree, :error) == :error
+    tree = PriceTree.new()
+    assert PriceTree.get_first(tree) == nil
+    assert PriceTree.get_first(tree, :error) == :error
 
-    assert AVLTree.get_last(tree) == nil
-    assert AVLTree.get_last(tree, :error) == :error
+    assert PriceTree.get_last(tree) == nil
+    assert PriceTree.get_last(tree, :error) == :error
   end
 
   test "get lower/upper" do
     tree =
       [{5, 50}, {1, 10}, {9, 90}, {1, 20}, {5, 40}, {7, 70}, {2, 20}]
-      |> Enum.into(AVLTree.new(fn {k1, _}, {k2, _} -> k1 < k2 end))
+      |> Enum.into(PriceTree.new(fn {k1, _}, {k2, _} -> k1 < k2 end))
 
-    assert AVLTree.get_lower(tree, {1, nil}) == {1, 10}
-    assert AVLTree.get_upper(tree, {1, nil}) == {1, 20}
+    assert PriceTree.get_lower(tree, {1, nil}) == {1, 10}
+    assert PriceTree.get_upper(tree, {1, nil}) == {1, 20}
 
-    assert AVLTree.get_lower(tree, {5, nil}) == {5, 50}
-    assert AVLTree.get_upper(tree, {5, nil}) == {5, 40}
+    assert PriceTree.get_lower(tree, {5, nil}) == {5, 50}
+    assert PriceTree.get_upper(tree, {5, nil}) == {5, 40}
 
-    assert AVLTree.get_lower(tree, {6, nil}) == nil
-    assert AVLTree.get_upper(tree, {6, nil}) == nil
+    assert PriceTree.get_lower(tree, {6, nil}) == nil
+    assert PriceTree.get_upper(tree, {6, nil}) == nil
 
-    assert AVLTree.get_lower(tree, {6, nil}, :error) == :error
-    assert AVLTree.get_upper(tree, {6, nil}, :error) == :error
+    assert PriceTree.get_lower(tree, {6, nil}, :error) == :error
+    assert PriceTree.get_upper(tree, {6, nil}, :error) == :error
   end
 
   test "put/delete lower/upper" do
     tree =
       [{1, 10}, {5, 50}, {9, 90}, {7, 70}, {2, 20}]
-      |> Enum.into(AVLTree.new(fn {k1, _}, {k2, _} -> k1 < k2 end))
+      |> Enum.into(PriceTree.new(fn {k1, _}, {k2, _} -> k1 < k2 end))
 
     assert Enum.to_list(tree) == [{1, 10}, {2, 20}, {5, 50}, {7, 70}, {9, 90}]
-    assert AVLTree.size(tree) == 5
-    assert AVLTree.height(tree) == 3
+    assert PriceTree.size(tree) == 5
+    assert PriceTree.height(tree) == 3
 
     #         {5, 50}
     #    ┌───────┴───────┐
@@ -147,10 +148,10 @@ defmodule AVLTreeTest do
              {{5, 50}, 3, {{1, 10}, 2, nil, {{2, 20}, 1, nil, nil}},
               {{9, 90}, 2, {{7, 70}, 1, nil, nil}, nil}}
 
-    tree = AVLTree.put(tree, {7, 700})
+    tree = PriceTree.put(tree, {7, 700})
     assert Enum.to_list(tree) == [{1, 10}, {2, 20}, {5, 50}, {7, 700}, {9, 90}]
-    assert AVLTree.size(tree) == 5
-    assert AVLTree.height(tree) == 3
+    assert PriceTree.size(tree) == 5
+    assert PriceTree.height(tree) == 3
 
     #         {5, 50}
     #    ┌───────┴────────┐
@@ -161,12 +162,12 @@ defmodule AVLTreeTest do
              {{5, 50}, 3, {{1, 10}, 2, nil, {{2, 20}, 1, nil, nil}},
               {{9, 90}, 2, {{7, 700}, 1, nil, nil}, nil}}
 
-    assert AVLTree.put_lower(tree, {8, 80}) == AVLTree.put_upper(tree, {8, 80})
-    assert AVLTree.put_lower(tree, {8, 80}) == AVLTree.put(tree, {8, 80})
+    assert PriceTree.put_lower(tree, {8, 80}) == PriceTree.put_upper(tree, {8, 80})
+    assert PriceTree.put_lower(tree, {8, 80}) == PriceTree.put(tree, {8, 80})
 
-    tree = AVLTree.put_lower(tree, {7, 71})
-    tree = AVLTree.put_lower(tree, {7, 72})
-    tree = AVLTree.put_lower(tree, {7, 73})
+    tree = PriceTree.put_lower(tree, {7, 71})
+    tree = PriceTree.put_lower(tree, {7, 72})
+    tree = PriceTree.put_lower(tree, {7, 73})
 
     assert Enum.to_list(tree) == [
              {1, 10},
@@ -179,8 +180,8 @@ defmodule AVLTreeTest do
              {9, 90}
            ]
 
-    assert AVLTree.size(tree) == 8
-    assert AVLTree.height(tree) == 4
+    assert PriceTree.size(tree) == 8
+    assert PriceTree.height(tree) == 4
 
     #         {5, 50}
     #    ┌───────┴───────────────┐
@@ -194,9 +195,9 @@ defmodule AVLTreeTest do
               {{7, 700}, 3, {{7, 72}, 2, {{7, 73}, 1, nil, nil}, {{7, 71}, 1, nil, nil}},
                {{9, 90}, 1, nil, nil}}}
 
-    tree = AVLTree.put_upper(tree, {7, 74})
-    tree = AVLTree.put_upper(tree, {7, 75})
-    tree = AVLTree.put_upper(tree, {7, 76})
+    tree = PriceTree.put_upper(tree, {7, 74})
+    tree = PriceTree.put_upper(tree, {7, 75})
+    tree = PriceTree.put_upper(tree, {7, 76})
 
     assert Enum.to_list(tree) == [
              {1, 10},
@@ -225,7 +226,7 @@ defmodule AVLTreeTest do
                {{7, 72}, 2, {{7, 73}, 1, nil, nil}, {{7, 71}, 1, nil, nil}}},
               {{7, 75}, 3, {{7, 74}, 1, nil, nil}, {{9, 90}, 2, {{7, 76}, 1, nil, nil}, nil}}}
 
-    tree = AVLTree.delete_upper(tree, {7, nil})
+    tree = PriceTree.delete_upper(tree, {7, nil})
 
     #                        {7, 700}
     #            ┌───────────────┴───────┐
@@ -240,7 +241,7 @@ defmodule AVLTreeTest do
                {{7, 72}, 2, {{7, 73}, 1, nil, nil}, {{7, 71}, 1, nil, nil}}},
               {{7, 75}, 2, {{7, 74}, 1, nil, nil}, {{9, 90}, 1, nil, nil}}}
 
-    tree = AVLTree.delete_lower(tree, {7, nil})
+    tree = PriceTree.delete_lower(tree, {7, nil})
 
     #                    {7, 700}
     #            ┌───────────┴───────┐
@@ -255,7 +256,7 @@ defmodule AVLTreeTest do
                {{7, 72}, 2, nil, {{7, 71}, 1, nil, nil}}},
               {{7, 75}, 2, {{7, 74}, 1, nil, nil}, {{9, 90}, 1, nil, nil}}}
 
-    tree = AVLTree.delete_lower(tree, {7, nil})
+    tree = PriceTree.delete_lower(tree, {7, nil})
 
     #                {7, 700}
     #            ┌───────┴───────┐
@@ -269,7 +270,7 @@ defmodule AVLTreeTest do
               {{5, 50}, 3, {{1, 10}, 2, nil, {{2, 20}, 1, nil, nil}}, {{7, 71}, 1, nil, nil}},
               {{7, 75}, 2, {{7, 74}, 1, nil, nil}, {{9, 90}, 1, nil, nil}}}
 
-    tree = AVLTree.delete_lower(tree, {7, nil})
+    tree = PriceTree.delete_lower(tree, {7, nil})
 
     #            {7, 700}
     #        ┌───────┴───────┐
@@ -280,7 +281,7 @@ defmodule AVLTreeTest do
              {{7, 700}, 3, {{2, 20}, 2, {{1, 10}, 1, nil, nil}, {{5, 50}, 1, nil, nil}},
               {{7, 75}, 2, {{7, 74}, 1, nil, nil}, {{9, 90}, 1, nil, nil}}}
 
-    tree = AVLTree.delete_upper(tree, {7, nil})
+    tree = PriceTree.delete_upper(tree, {7, nil})
 
     #            {7, 700}
     #        ┌───────┴───┐
@@ -292,18 +293,18 @@ defmodule AVLTreeTest do
              {{7, 700}, 3, {{2, 20}, 2, {{1, 10}, 1, nil, nil}, {{5, 50}, 1, nil, nil}},
               {{7, 74}, 2, nil, {{9, 90}, 1, nil, nil}}}
 
-    assert ^tree = AVLTree.delete_upper(tree, {6, nil})
-    assert ^tree = AVLTree.delete_lower(tree, {6, nil})
+    assert ^tree = PriceTree.delete_upper(tree, {6, nil})
+    assert ^tree = PriceTree.delete_lower(tree, {6, nil})
   end
 
   test "delete" do
-    tree = Enum.into([5, 9, 3, 8, 1, 6, 7], AVLTree.new())
-    assert match?(^tree, AVLTree.delete(tree, 4))
-    tree = AVLTree.delete(tree, 3)
+    tree = Enum.into([5, 9, 3, 8, 1, 6, 7], PriceTree.new())
+    assert match?(^tree, PriceTree.delete(tree, 4))
+    tree = PriceTree.delete(tree, 3)
 
     assert Enum.to_list(tree) == [1, 5, 6, 7, 8, 9]
-    assert AVLTree.size(tree) == 6
-    assert AVLTree.height(tree) == 3
+    assert PriceTree.size(tree) == 6
+    assert PriceTree.height(tree) == 3
 
     #    6
     #  ┌─┴─┐
@@ -313,9 +314,9 @@ defmodule AVLTreeTest do
     assert tree.root ==
              {6, 3, {5, 2, {1, 1, nil, nil}, nil}, {8, 2, {7, 1, nil, nil}, {9, 1, nil, nil}}}
 
-    tree = AVLTree.delete(tree, 1)
+    tree = PriceTree.delete(tree, 1)
     assert Enum.to_list(tree) == [5, 6, 7, 8, 9]
-    assert AVLTree.size(tree) == 5 and AVLTree.height(tree) == 3
+    assert PriceTree.size(tree) == 5 and PriceTree.height(tree) == 3
 
     #  6
     # ┌┴─┐
@@ -324,9 +325,9 @@ defmodule AVLTreeTest do
     #   7 9
     assert tree.root == {6, 3, {5, 1, nil, nil}, {8, 2, {7, 1, nil, nil}, {9, 1, nil, nil}}}
 
-    tree = AVLTree.delete(tree, 8)
+    tree = PriceTree.delete(tree, 8)
     assert Enum.to_list(tree) == [5, 6, 7, 9]
-    assert AVLTree.size(tree) == 4 and AVLTree.height(tree) == 3
+    assert PriceTree.size(tree) == 4 and PriceTree.height(tree) == 3
 
     #  6
     # ┌┴─┐
@@ -337,7 +338,7 @@ defmodule AVLTreeTest do
 
     tree = Enum.into([3, 1, 2, 4, 8, 0], tree)
     assert Enum.to_list(tree) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    assert AVLTree.size(tree) == 10 and AVLTree.height(tree) == 4
+    assert PriceTree.size(tree) == 10 and PriceTree.height(tree) == 4
 
     #        6
     #    ┌───┴─┐
@@ -351,9 +352,9 @@ defmodule AVLTreeTest do
               {3, 3, {1, 2, {0, 1, nil, nil}, {2, 1, nil, nil}}, {5, 2, {4, 1, nil, nil}, nil}},
               {8, 2, {7, 1, nil, nil}, {9, 1, nil, nil}}}
 
-    tree = AVLTree.delete(tree, 6)
+    tree = PriceTree.delete(tree, 6)
     assert Enum.to_list(tree) == [0, 1, 2, 3, 4, 5, 7, 8, 9]
-    assert AVLTree.size(tree) == 9 and AVLTree.height(tree) == 4
+    assert PriceTree.size(tree) == 9 and PriceTree.height(tree) == 4
 
     #      5
     #    ┌─┴─┐
@@ -366,10 +367,10 @@ defmodule AVLTreeTest do
              {5, 4, {3, 3, {1, 2, {0, 1, nil, nil}, {2, 1, nil, nil}}, {4, 1, nil, nil}},
               {8, 2, {7, 1, nil, nil}, {9, 1, nil, nil}}}
 
-    tree = Enum.into([0, 3, 2, 1, 4, 8, 7, 9, 5, 6, 11, 12, 10, 14, 13, 15], AVLTree.new())
-    tree = AVLTree.delete(tree, 4)
+    tree = Enum.into([0, 3, 2, 1, 4, 8, 7, 9, 5, 6, 11, 12, 10, 14, 13, 15], PriceTree.new())
+    tree = PriceTree.delete(tree, 4)
     assert Enum.to_list(tree) == [0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    assert AVLTree.size(tree) == 15 and AVLTree.height(tree) == 5
+    assert PriceTree.size(tree) == 15 and PriceTree.height(tree) == 5
 
     #      5
     #    ┌─┴──────┐
@@ -385,10 +386,10 @@ defmodule AVLTreeTest do
               {11, 4, {7, 3, {6, 1, nil, nil}, {9, 2, {8, 1, nil, nil}, {10, 1, nil, nil}}},
                {13, 3, {12, 1, nil, nil}, {14, 2, nil, {15, 1, nil, nil}}}}}
 
-    tree = tree |> AVLTree.delete(3) |> AVLTree.delete(2)
+    tree = tree |> PriceTree.delete(3) |> PriceTree.delete(2)
     assert Enum.to_list(tree) == [0, 1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-    assert AVLTree.size(tree) == 13 and AVLTree.height(tree) == 5
+    assert PriceTree.size(tree) == 13 and PriceTree.height(tree) == 5
 
     #          11
     #    ┌──────┴──┐
@@ -409,9 +410,9 @@ defmodule AVLTreeTest do
   test "view" do
     tree =
       [{5, 50}, {1, 10}, {9, 90}, {1, 20}, {5, 40}, {6, 60}, {7, 70}, {2, 20}]
-      |> Enum.into(AVLTree.new(fn {k1, _}, {k2, _} -> k1 < k2 end))
+      |> Enum.into(PriceTree.new(fn {k1, _}, {k2, _} -> k1 < k2 end))
 
-    assert AVLTree.view(tree) ==
+    assert PriceTree.view(tree) ==
              Enum.join(
                [
                  "            {5, 50}                ",
--- elixir/test/order_book_test.exs
+++ elixir/test/order_book_test.exs
@@ -1,8 +1,122 @@
 defmodule OrderBookTest do
   use ExUnit.Case
   doctest OrderBook
+  alias OrderBook.{Order, PriceTree, OrderQueue}
 
-  test "greets the world" do
-    assert OrderBook.hello() == :world
+  test "get_order_id/1 returns a new order id, increments last_order_id" do
+    ob = OrderBook.new()
+    assert ob.last_order_id == 0
+    assert {1, %OrderBook{last_order_id: 1}} = OrderBook.get_order_id(ob)
   end
+
+  test "get_transaction_id/1 returns a new transaction id, increments last_transaction_id" do
+    ob = OrderBook.new()
+    assert ob.last_transaction_id == 0
+    assert {1, %OrderBook{last_transaction_id: 1}} = OrderBook.get_transaction_id(ob)
+  end
+
+  test "OrderBook.new() bids and asks start out as empty AVL trees" do
+    ob = OrderBook.new()
+    assert PriceTree.is_empty?(ob.asks)
+    assert PriceTree.is_empty?(ob.bids)
+  end
+
+  test "insert_active_order/2 adds an order to the book's active_orders" do
+    ob = OrderBook.new()
+    order_1 = %Order{id: 1}
+    order_2 = %Order{id: 2}
+
+    ob = ob
+         |> OrderBook.insert_active_order(order_1)
+         |> OrderBook.insert_active_order(order_2)
+
+    assert Map.has_key?(ob.active_orders, order_1.id)
+    assert Map.has_key?(ob.active_orders, order_2.id)
+  end
+
+  test "get_active_order/2 returns an active order" do
+    ob = OrderBook.new()
+    order_1 = %Order{id: 1}
+    order_2 = %Order{id: 2}
+
+    ob = ob
+         |> OrderBook.insert_active_order(order_1)
+         |> OrderBook.insert_active_order(order_2)
+
+    assert OrderBook.get_active_order(ob, 1) == order_1
+    assert OrderBook.get_active_order(ob, 2) == order_2
+  end
+
+  test "remove_active_order/2 removes an order from the book's active_orders" do
+    ob = OrderBook.new()
+    order_1 = %Order{id: 1}
+    order_2 = %Order{id: 2}
+
+    ob = ob
+         |> OrderBook.insert_active_order(order_1)
+         |> OrderBook.insert_active_order(order_2)
+         |> OrderBook.remove_active_order(order_1)
+
+    refute Map.has_key?(ob.active_orders, order_1.id)
+    assert Map.has_key?(ob.active_orders, order_2.id)
+  end
+
+  test "remove_active_order/2 returns an unmodified book if no order is found to remove" do
+    ob = OrderBook.new()
+    order_1 = %Order{id: 1}
+    fake_order = %Order{id: 2}
+
+    ob = ob
+         |> OrderBook.insert_active_order(order_1)
+         |> OrderBook.remove_active_order(fake_order)
+
+    assert Map.has_key?(ob.active_orders, order_1.id)
+  end
+
+  test "update_active_order/4 updates an order in a book's active_orders" do
+    ob = OrderBook.new()
+    order_1 = %Order{id: 1}
+
+    ob = 
+      ob
+      |> OrderBook.insert_active_order(order_1)
+      |> OrderBook.update_active_order(order_1, :qty, fn _ -> 500 end)
+      |> OrderBook.update_active_order(order_1, :price, &(&1 + 1500))
+
+    updated_order = OrderBook.get_active_order(ob, 1)
+    assert updated_order.qty == 500
+    assert updated_order.price == 1500
+  end
+
+  test "queue_active_order/2 correctly enqueues a bid order id at a price level" do
+    ob = OrderBook.new()
+    order_1 = %Order{id: 1, side: :bid, price: 500, qty: 10}
+
+    ob =
+      ob
+      |> OrderBook.insert_active_order(order_1)
+      |> OrderBook.enqueue_active_order(order_1)
+
+
+    assert PriceTree.has_price?(ob.bids, order_1.price)
+    assert %OrderQueue{} = PriceTree.get_order_queue(ob.bids, order_1.price)
+    assert OrderQueue.peek(PriceTree.get_order_queue(ob.bids, order_1.price)) == 1
+  end
+
+  test "enqueue_active_order/2 correctly enqueues an ask order id at a price level" do
+    ob = OrderBook.new()
+    order_1 = %Order{id: 1, side: :ask, price: 500, qty: 10}
+
+    ob =
+      ob
+      |> OrderBook.insert_active_order(order_1)
+      |> OrderBook.enqueue_active_order(order_1)
+
+
+    assert PriceTree.has_price?(ob.asks, order_1.price)
+    assert %OrderQueue{} = PriceTree.get_order_queue(ob.asks, order_1.price)
+    assert OrderQueue.peek(PriceTree.get_order_queue(ob.asks, order_1.price)) == 1
+  end
+
+
 end
